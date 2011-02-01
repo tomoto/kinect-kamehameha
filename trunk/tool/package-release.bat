0c1b389416a41fa5d0286ce48573997161fe3bd2
tomoto@gmail.com <tomoto@gmail.com@bc77eb07-9f00-74ed-c11c-046e0332cb39>
2011-02-01 07:48:15 +0000

    First release

---
+++ trunk/tool/package-release.bat
@@ -0,0 +1,14 @@
+@rem Note: (1) Set VER to release version. (2) Install ActivePerl.
+
+setlocal
+set VER=0.1a
+set TARGET=release\kinect-kamehameha-%VER%
+rmdir /s /q %TARGET%
+mkdir %TARGET%
+xcopy ..\data %TARGET%\data\ /e
+copy ..\build\release\kinect-kamehameha.exe %TARGET%\
+copy ..\build\release\cxcore210.dll %TARGET%
+copy ..\LICENSE.TXT %TARGET%
+copy ..\README_*.TXT %TARGET%
+copy ..\HISTORY_*.TXT %TARGET%
+perl zip.pl %TARGET%
