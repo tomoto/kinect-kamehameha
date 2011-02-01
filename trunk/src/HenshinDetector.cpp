60212dc7e39de72debcc7c665108f31991102c0f
tomoto@gmail.com <tomoto@gmail.com@bc77eb07-9f00-74ed-c11c-046e0332cb39>
2011-02-01 07:42:55 +0000

    First release

---
+++ trunk/src/HenshinDetector.cpp
@@ -0,0 +1,105 @@
+//@COPYRIGHT@//
+//
+// Copyright (c) 2011, Tomoto S. Washio
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Tomoto S. Washio nor the names of his
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//@COPYRIGHT@//
+
+#include "HenshinDetector.h"
+#include "util.h"
+
+HenshinDetector::HenshinDetector(UserDetector* userDetector) : AbstractPoseDetector(userDetector)
+{
+	m_userDetector->addListener(this);
+	transitToHuman();
+}
+
+HenshinDetector::~HenshinDetector()
+{
+}
+
+void HenshinDetector::transitToHuman()
+{
+	printf("Henshin Stage = HUMAN\n");
+	m_stage = STAGE_HUMAN;
+	m_henshinProgress = 0;
+	m_userDetector->stopTracking();
+}
+
+void HenshinDetector::transitToHenshined()
+{
+	printf("Henshin Stage = HENSHINED\n");
+	m_stage = STAGE_HENSHINED;
+	m_henshinProgress = 0;
+}
+
+bool HenshinDetector::isPosing(float dt)
+{
+	if (m_stage == STAGE_HENSHINED && m_henshinProgress < 1.0f) { 
+		m_henshinProgress = std::min(m_henshinProgress + 0.5f * dt, 1.0f);
+	}
+
+	return false;
+}
+
+void HenshinDetector::onPoseDetected(float dt)
+{
+}
+
+//
+// listener methods
+//
+
+void HenshinDetector::onNewUser(XnUserID userID)
+{
+}
+
+void HenshinDetector::onLostUser(XnUserID userID)
+{
+	if (m_userDetector->isTrackedUserID(userID)) {
+		transitToHuman();
+	}
+}
+
+void HenshinDetector::onCalibrationStart(XnUserID userID)
+{
+}
+
+void HenshinDetector::onCalibrationEnd(XnUserID userID, bool isSuccess)
+{
+	if (isSuccess) {
+		transitToHenshined();
+	} else {
+		transitToHuman();
+	}
+}
+
+void HenshinDetector::onPoseStart(XnUserID userID, const XnChar* pose)
+{
+}
+
+void HenshinDetector::onPoseEnd(XnUserID userID, const XnChar* pose)
+{
+}
---
+++ trunk/src/HenshinDetector.h
@@ -0,0 +1,77 @@
+//@COPYRIGHT@//
+//
+// Copyright (c) 2011, Tomoto S. Washio
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Tomoto S. Washio nor the names of his
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//@COPYRIGHT@//
+
+#ifndef _HENSHIN_DETECTOR_H_
+#define _HENSHIN_DETECTOR_H_
+
+#include "common.h"
+#include "AbstractPoseDetector.h"
+#include "UserListener.h"
+#include "UserDetector.h"
+
+class HenshinDetector : public AbstractPoseDetector, UserListener
+{
+public:
+	enum Stage {
+		STAGE_HUMAN,
+		STAGE_HENSHINED
+	};
+
+private:
+	Stage m_stage;
+	float m_henshinProgress;
+
+public:
+	HenshinDetector(UserDetector* userDetector);
+	virtual ~HenshinDetector();
+
+	UserDetector* getUserDetector() { return m_userDetector; }
+	HenshinDetector::Stage getStage() { return m_stage; }
+	float getHenshinProgress() { return m_henshinProgress; }
+
+	virtual bool isPosing(float dt);
+	virtual void onPoseDetected(float dt);
+
+	//
+	// user listener methods
+	//
+
+	virtual void onNewUser(XnUserID userID);
+	virtual void onLostUser(XnUserID userID);
+	virtual void onCalibrationStart(XnUserID userID);
+	virtual void onCalibrationEnd(XnUserID userID, bool isSuccess);
+	virtual void onPoseStart(XnUserID userID, const XnChar* pose);
+	virtual void onPoseEnd(XnUserID userID, const XnChar* pose);
+
+private:
+	void transitToHuman();
+	void transitToHenshined();
+};
+
+#endif
