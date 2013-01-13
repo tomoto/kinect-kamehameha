54a0418009b21db17a3e03d9569fc406977f198b
tomoto <tomoto@bc77eb07-9f00-74ed-c11c-046e0332cb39>
2013-01-13 03:26:27 +0000

    Added release packager for OpenNI2.

--- tool/package-release.bat
+++ tool/package-release.bat
@@ -24,3 +24,20 @@ copy ..\LICENSE.TXT %TARGET%
 copy ..\README_*.TXT %TARGET%
 copy ..\HISTORY_*.TXT %TARGET%
 perl zip.pl %TARGET%
+
+set TARGET=release\kinect-kamehameha_%VER%_for_OpenNI2
+rmdir /s /q %TARGET%
+mkdir %TARGET%
+xcopy ..\data %TARGET%\data\ /e
+copy ..\build2010\OpenNI2_Release\kinect-kamehameha.exe %TARGET%\
+copy ..\build2010\OpenNI2_Release\opencv_*.dll %TARGET%
+copy ..\LICENSE.TXT %TARGET%
+copy ..\README_*.TXT %TARGET%
+copy ..\HISTORY_*.TXT %TARGET%
+copy ..\OpenNI* %TARGET%
+copy ..\PS1080* %TARGET%
+copy ..\NiTE* %TARGET%
+xcopy ..\OpenNI2 %TARGET%\OpenNI2\ /e
+xcopy ..\NiTE2 %TARGET%\NiTE2\ /e
+perl zip.pl %TARGET%
+
